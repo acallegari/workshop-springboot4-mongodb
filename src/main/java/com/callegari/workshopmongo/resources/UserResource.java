ededbad6b8380096b215f62c39724ce055c7ae3c
Antonio Carlos Callegari <acallegari@gmail.com>
2026-07-13 15:12:10 -0300

    Atualização de usuário com PUT

--- src/main/java/com/callegari/workshopmongo/resources/UserResource.java
+++ src/main/java/com/callegari/workshopmongo/resources/UserResource.java
@@ -17,46 +17,51 @@ import com.callegari.workshopmongo.domain.User;
 import com.callegari.workshopmongo.dto.UserDTO;
 import com.callegari.workshopmongo.services.UserService;
 
-
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
-	
+
 	@Autowired
 	private UserService service;
-	
-	@RequestMapping(method=RequestMethod.GET)
+
+	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<UserDTO>> findAll() {
-		
+
 		List<User> list = service.findAll();
-		
+
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
-		
+
 		return ResponseEntity.ok().body(listDto);
-		
+
 	}
-	
-	@RequestMapping(value="/{id}",method=RequestMethod.GET)
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
 
-	@RequestMapping(method=RequestMethod.POST)
+	@RequestMapping(method = RequestMethod.POST)
 	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
 		User obj = service.fromDTO(objDto);
-		
 		service.insert(obj);
-		
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
-		
 		return ResponseEntity.created(uri).build();
 	}
 
-	@RequestMapping(value="/{id}",method=RequestMethod.DELETE)
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable String id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
 
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.insert(obj);
+		return ResponseEntity.noContent().build();
+	}
+	
+	
 }
