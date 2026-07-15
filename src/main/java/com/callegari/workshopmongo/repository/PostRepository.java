39c0af3b21c2907ba1d47a2dedd62c2c163ef105
Antonio Carlos Callegari <acallegari@gmail.com>
2026-07-15 10:35:46 -0300

    Consulta simples com @Query

--- src/main/java/com/callegari/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/callegari/workshopmongo/repository/PostRepository.java
@@ -3,11 +3,16 @@ package com.callegari.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.callegari.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchByTitle(String text);
 }
--- src/main/java/com/callegari/workshopmongo/services/PostService.java
+++ src/main/java/com/callegari/workshopmongo/services/PostService.java
@@ -22,6 +22,7 @@ public class PostService {
     	 return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado")); 
     	} 
    public List<Post> findByTitle(String text) {
-	   return repo.findByTitleContainingIgnoreCase(text);
+	   //return repo.findByTitleContainingIgnoreCase(text);
+	   return repo.searchByTitle(text);
    }
 }
